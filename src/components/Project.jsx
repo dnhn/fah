4dfcffe7cde915fc1fb03899fad5f2354051fa30
Nhan Doan <nhandev88@gmail.com>
2020-05-03 15:49:43 +0700

    Improve Project display conditions

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,15 +16,28 @@ export default ({ projectId }) => {
     <div className="Project Card">
       <h1 className="Card__Heading">Project {projectId}</h1>
       <section className="Card__Content">
-        <p><strong style={{ textTransform: 'capitalize' }}>{project.cause}</strong> project managed by <strong>{project.manager}</strong>{project.institution && (<> at <strong>{project.institution}</strong></>)}. {project.url &&
-          <a
-            href={project.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {project.url}
-          </a>
-        }</p>
+        <p>
+          {project.cause === 'unspecified' ?
+            'Managed by' :
+            (
+              <>
+                <strong style={{ textTransform: 'capitalize' }}>
+                  {project.cause}
+                </strong> project managed by
+              </>
+            )
+          } <strong>{project.manager}</strong>{
+            project.institution && (<> at <strong>{project.institution}</strong></>)
+          }. {project.url &&
+            <a
+              href={project.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {project.url}
+            </a>
+          }
+        </p>
         <div dangerouslySetInnerHTML={{ __html: project.description }} />
         <hr style={{ borderWidth: 1, borderStyle: 'solid' }} />
         <div dangerouslySetInnerHTML={{ __html: project.mdescription }} />
