b1ba7c92fa18c0d58affcb4703cb1fef10f54954
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2025-02-06 23:52:56 +0700

    Setup react-query

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import reactQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -12,6 +13,7 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
+      ...reactQuery.configs['flat/recommended']
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
---
+++ src/providers/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
+export default function Providers({ children }: {children: ReactNode}) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition='bottom-left' />
+    </QueryClientProvider>
+  );
+}
