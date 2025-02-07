77088455a94822c25fc09e2b5648098a432cb318
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2025-02-07 15:35:20 +0700

    Upgrade Lambda function

---
+++ lambda/getWorkStatus.mjs
@@ -0,0 +1,24 @@
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+
+const dynamo = new DynamoDBClient({
+  credentials: {
+    accessKeyId: Netlify.env.get('FAH_AWS_ACCESS_KEY'),
+    secretAccessKey: Netlify.env.get('FAH_AWS_SECRET_KEY'),
+  },
+  region: Netlify.env.get('FAH_AWS_REGION'),
+});
+
+const command = new GetItemCommand({
+  TableName: Netlify.env.get('FAH_AWS_DYNAMO_TABLE'),
+  Key: { id: 0 },
+});
+
+export default async () => {
+  try {
+    const data = await dynamo.send(command)
+    return new Response(data.Item);
+  } catch (error) {
+    console.error(error);
+    return new Response(error);
+  }
+};
