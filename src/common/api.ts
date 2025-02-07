eb6f3016f9acaa5ec67e501e2a1b4423a32d8d75
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2025-02-07 15:47:37 +0700

    Clean up code

--- src/common/api.ts
+++
@@ -1,9 +0,0 @@
-const mainUrl = 'https://api.foldingathome.org'
-
-export const endpoints = {
-  GET_PROJECT: (id: number) => `${mainUrl}/project/${id}`,
-  GET_TEAM: (id: number) => `${mainUrl}/team/${id}`,
-  GET_USER_BY_ID: (id: number) => `${mainUrl}/uid/${id}`,
-  GET_USER_BY_USERNAME: (username: string) => `${mainUrl}/user/${username}`,
-  GET_WORK_STATUS: '/.netlify/functions/getWorkStatus',
-};
