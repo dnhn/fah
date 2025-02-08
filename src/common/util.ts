2170cc909f236e8cffde1e3fc3f7ed3e7acdc160
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2025-02-09 01:48:28 +0700

    Locale

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,7 +1,9 @@
-export const formatNumber = (n: number) => new Intl.NumberFormat('en-GB').format(n);
+import { LOCALE } from './config';
+
+export const formatNumber = (n: number) => new Intl.NumberFormat(LOCALE).format(n);
 
 export const formatDate = (d: number | string | Date) => new Date(d)
-  .toLocaleDateString('en-GB', {
+  .toLocaleDateString(LOCALE, {
     year: 'numeric',
     month: 'long',
     day: 'numeric'
