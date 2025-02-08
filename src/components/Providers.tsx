1180f4923ec7275e7e10dec220c7a39a043ead40
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2025-02-09 04:16:58 +0700

    Set query stale time

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,7 +2,13 @@ import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 3600000
+    }
+  }
+})
 
 export default function Providers({ children }: {children: ReactNode}) {
   return (
